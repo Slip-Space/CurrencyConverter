5d6d40517b1ebdcf54f20dc81825680ead5df97d
Kobie Danquah <devkobie815@gmail.com>
2022-08-04 06:37:58 +0000

    home screen added

---
+++ app/src/main/java/dev/freekobie/currencyconverter/CompareActivity.java
@@ -0,0 +1,14 @@
+package dev.freekobie.currencyconverter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CompareActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_compare);
+    }
+}
